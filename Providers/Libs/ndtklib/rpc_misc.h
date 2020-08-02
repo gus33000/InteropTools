7d640f40f48e3f37ae3b4b530b61d24795e916eb
Gustave Monce <gustave.monce@outlook.com>
2020-08-02 22:26:23 +0200

    Updates

--- Providers/Libs/ndtklib/rpc_misc.h
+++ Providers/Libs/ndtklib/rpc_misc.h
@@ -61,30 +61,6 @@ typedef struct {
 	RPC_SECURITY_QOS        *SecurityQos;
 }  RPC_BINDING_HANDLE_SECURITY;
 
-typedef struct _RPC_BINDING_HANDLE_TEMPLATE_V1_W {
-	unsigned long Version;
-	unsigned long Flags;
-	unsigned long ProtocolSequence;
-	unsigned short *NetworkAddress;
-	unsigned short *StringEndpoint;
-	union
-	{
-		unsigned short *Reserved;
-	} u1;
-	UUID ObjectUuid;
-} RPC_BINDING_HANDLE_TEMPLATE_V1_W, *PRPC_BINDING_HANDLE_TEMPLATE_V1_W;
-
-
-
-typedef struct _RPC_BINDING_HANDLE_SECURITY_V1_W {
-	unsigned long Version;
-	unsigned short *ServerPrincName;
-	unsigned long AuthnLevel;
-	unsigned long AuthnSvc;
-	SEC_WINNT_AUTH_IDENTITY_W *AuthIdentity;
-	RPC_SECURITY_QOS *SecurityQos;
-} RPC_BINDING_HANDLE_SECURITY_V1_W, *PRPC_BINDING_HANDLE_SECURITY_V1_W;
-
 typedef struct _STARTUPINFO {
 	DWORD  cb;
 	LPTSTR lpReserved;
@@ -112,39 +88,6 @@ typedef struct _PROCESS_INFORMATION {
 	DWORD  dwProcessId;
 	DWORD  dwThreadId;
 } PROCESS_INFORMATION, *LPPROCESS_INFORMATION;
-
-
-
-
-typedef enum _RPC_ASYNC_EVENT {
-	RpcCallComplete,
-	RpcSendComplete,
-	RpcReceiveComplete,
-	RpcClientDisconnect,
-	RpcClientCancel
-} RPC_ASYNC_EVENT;
-
-
-typedef struct _RPC_ASYNC_STATE {
-	unsigned int                Size;
-	unsigned long               Signature;
-	long                        Lock;
-	unsigned long               Flags;
-	void                        *StubInfo;
-	void                        *UserInfo;
-	void                        *RuntimeInfo;
-	RPC_ASYNC_EVENT             Event;
-	void      *NotificationType;
-	void *u;
-	LONG_PTR                    Reserved[4];
-} RPC_ASYNC_STATE, *PRPC_ASYNC_STATE;
-
-typedef struct _RPC_BINDING_HANDLE_OPTIONS_V1 {
-	unsigned long Version;
-	unsigned long Flags;
-	unsigned long ComTimeout;
-	unsigned long CallTimeout;
-} RPC_BINDING_HANDLE_OPTIONS_V1, RPC_BINDING_HANDLE_OPTIONS;
 
 extern handle_t hello_IfHandle;
 extern const hello_MIDL_TYPE_FORMAT_STRING hello__MIDL_TypeFormatString;
